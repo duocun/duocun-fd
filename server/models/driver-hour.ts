d58ffa64cea637794ce011de3a070f36521202ef
zlkca <likzhang@gmail.com>
2020-05-07 08:22:37 -0400

    remove server folder and update package.json

--- server/models/driver-hour.ts
+++
@@ -1,8 +0,0 @@
-import { DB } from "../db";
-import { Model } from "./model";
-
-export class DriverHour extends Model {
-  constructor(dbo: DB) {
-    super(dbo, 'driver_hours');
-  }
-}
--- server/models/pickup.ts
+++
@@ -1,8 +0,0 @@
-import { DB } from "../db";
-import { Model } from "./model";
-
-export class Pickup extends Model {
-  constructor(dbo: DB) {
-    super(dbo, 'pickups');
-  }
-}
